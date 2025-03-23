fcceba4aab2ca6e2b5cebdb9c2b92f9d75d9cde1
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-03-23 14:48:12 +0630

    context and reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Basket from "./pages/Basket";
+import { StoreProvider } from "./context-and-reducer/StoreContext";
 
 function App() {
   return (
-    <>
+    <StoreProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basket" element={<Basket />} />
         </Routes>
       </Router>
-    </>
+    </StoreProvider>
   );
 }
 
